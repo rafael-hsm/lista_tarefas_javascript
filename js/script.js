434ab877f278b78c49efd7150d689e90baaa517a
Rafael Meireles <rafael.meireles.prestserv@petrobras.com.br>
2023-08-20 10:44:24 -0300

    update finish project

--- js/script.js
+++ js/script.js
@@ -7,21 +7,27 @@ let btn = document.querySelector('#botao');
 // 3) Temos que referenciar a lista
 let lista = document.querySelector('#lista');
 
-let tarefas = [
-    'Jogar GTA5',
-    'Estudar Python',
-    'Estudar JavaScript',
-    'Ver um filme',
-    'Ler um livro',
-];
+// card
+let card = document.querySelector('.card');
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
 function renderizarTarefas(){
+
+    // Limpar a listagem de itens antes de renderizar novamente a tela
+    lista.innerHTML = '';
+
     for(tarefa of tarefas){
         //Criar o item da lista
         let itemLista = document.createElement('li');
 
         // Adicionar classes no item da lista
         itemLista.setAttribute('class', 'list-group-item list-group-item-action');
+
+        // Adicionar evento de clique no item da Lista
+        itemLista.onclick = function(){
+            deletarTarefa(this);
+        }
 
         // Criar um texto
         let itemTexto = document.createTextNode(tarefa);
@@ -42,9 +48,64 @@ btn.onclick = function(){
     // 2) Precisamos capturar o valor digitado pelo usuario no input
     let novaTarefa = input.value;
 
-    // 3) Precisamos atualizar a nova tarefa na lista (array) de tarefas e renderizar a tela
-    tarefas.push(novaTarefa)
+    if(novaTarefa !== ''){
+        // 3) Precisamos atualizar a nova tarefa na lista (array) de tarefas e renderizar a tela
+        tarefas.push(novaTarefa)
 
-    //Executando a funcao para renderizar as tarefas
-    renderizarTarefas();
+        //Executando a funcao para renderizar as tarefas
+        renderizarTarefas();
+
+        // Limpart o input
+        input.value = ''
+
+        // Limpar mensagens de erro (spans)
+        removerSpans();
+
+        // Salva os novos dados no banco de dados
+        salvarDadosNoStorage();
+    } else {
+        // Limpar mensagens de erro (spans)
+        removerSpans();
+
+        let span = document.createElement('span');
+        span.setAttribute('class', 'alert alert-warning');
+
+        let msg = document.createTextNode('Você precisa informar a tarefa!');
+
+        span.appendChild(msg);
+
+        card.appendChild(span);
+    }
+
 }
+
+function removerSpans(){
+    let spans = document.querySelectorAll('span');
+
+    for(let i = 0; i < spans.length; i++){
+        card.removeChild(spans[i]);
+    }
+}
+
+function deletarTarefa(tar){
+    // console.log("Parâmetro: tar", tar);
+    // console.log("tar.textContent: ",tar.textContent)
+    // console.log("Index de tar dentro de tarefas: ", tarefas.indexOf(tar.textContent))
+
+    // Remove a tarefa do array
+    tarefas.splice(tarefas.indexOf(tar.textContent), 1);
+
+    // Renderiza novamente a tela
+    renderizarTarefas();
+
+    // Salva os novos dados no banco de dados
+    salvarDadosNoStorage();
+}
+
+function salvarDadosNoStorage(){
+    // Todo navegador web possui esta capacidade
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+// console.log(tarefas)
+// console.log(JSON.stringify(tarefas))
